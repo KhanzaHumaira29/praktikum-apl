0f6e2ba56ac34ef20d6eccdd4a703a5e3533dcda
KhanzaHumaira29 <khanzahumaira824@gmail.com>
2026-02-27 09:15:26 +0800

    post test 1 apl

---
+++ kelas/pertemuan-1/pertemuan1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Pertemuan 1" << endl;
+    return 0;
+}
---
+++ post-test/post-test-1/2509106065_KHANZAHUMAIRA_PT-1.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    //variabel login
+    string username;
+    string passwordInput;
+    string passwordAsli = "065";
+    int jumlahPercobaan = 0;
+
+    //variabel menu
+    int pilihanMenu;
+
+    //variabel konversi
+    double nilaiJam, nilaiMenit, nilaiDetik;
+    double hasilJam, hasilMenit, hasilDetik;
+
+    //tampilan awal
+    cout << "\n";
+    cout << "  __________________________________________________\n";
+    cout << " |                                                  |\n";
+    cout << " |            Iconic Digital Time Converter         |\n";
+    cout << " |__________________________________________________|\n\n";
+
+    //login
+    while (jumlahPercobaan < 3) {
+
+        cout << " Silakan login untuk menggunakan Iconic Digital Time Converter\n";
+        cout << " Masukkan Username Anda : ";
+        cin >> username;
+        getline(cin, username);
+
+        cout << " Masukkan Password Anda (3 digit angka): ";
+        getline(cin, passwordInput);
+
+        if (passwordInput == passwordAsli) {
+            cout << "\nLogin Berhasil!\n\n";
+            break;
+        }
+        else {
+            jumlahPercobaan++;
+            cout << " Login gagal. Ulangi! Percobaan ke- " << jumlahPercobaan << endl;
+
+            if (jumlahPercobaan == 3) {
+                cout << "\n Maaf, sesi Anda login telah habis.\n";
+                cout << "Program Berhenti.\n";
+                return 0;
+            }
+        }
+        cout << endl;
+    }
+    //tampilan menu
+    do {
+        cout << "\n";
+        cout << "  -----------------------------------------------------------\n";
+        cout << " |                     MENU UTAMA                           |\n";
+        cout << "  -----------------------------------------------------------\n";
+        cout << "   [1] Jam    -> Menit & Detik\n";
+        cout << "   [2] Menit  -> Jam & Detik\n";
+        cout << "   [3] Detik  -> Jam & Menit\n";
+        cout << "   [4] Keluar\n";
+        cout << "  -----------------------------------------------------------\n";
+        cout << "   Masukkan Pilihan (1-4) : ";
+        cin >> pilihanMenu;
+
+        cout << endl;
+
+        switch (pilihanMenu) {
+
+            case 1:
+                cout << "   Masukkan jumlah jam yang ingin dikonversi : ";
+                cin >> nilaiJam;
+
+                hasilMenit = nilaiJam * 60;
+                hasilDetik = nilaiJam * 3600;
+
+                cout << "\n   >>> HASIL KONVERSI <<<\n";
+                cout << "   " << nilaiJam << " jam = "
+                    << hasilMenit << " menit dan "
+                    << hasilDetik << " detik\n";
+                break;
+            
+                case 2:
+                cout << "   Masukkan jumlah menit yang ingin dikonversi: ";
+                cin >> nilaiMenit;
+
+                hasilJam = nilaiMenit / 60;
+                hasilDetik = nilaiMenit * 60;
+
+                cout << "\n   >>> HASIL KONVERSI <<<\n";
+                cout << "   " << nilaiMenit << " menit = "
+                    << hasilJam << " jam dan "
+                    << hasilDetik << " detik\n";
+                break;
+
+                case 3:
+                cout << "   Masukkan jumlah detik yang ingin dikonversi: ";
+                cin >> nilaiDetik;
+
+                hasilJam = nilaiDetik / 3600;
+                hasilMenit = nilaiDetik / 60;
+
+                cout << "\n   >>> HASIL KONVERSI <<<\n";
+                cout << "   " << nilaiDetik << " detik = "
+                    << hasilJam << " jam dan "
+                    << hasilMenit << " menit\n";
+                break;
+
+                case 4:
+                cout << "\n   Terima kasih telah menggunakan Iconic Digital Time Converter.\n";
+                break;
+
+            default:
+                cout << "   Pilihan tidak valid!\n";
+        }
+
+        cin.ignore(); //refresh line
+
+    } while (pilihanMenu != 4);
+
+    return 0;
+}
